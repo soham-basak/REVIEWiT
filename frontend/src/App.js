6cb5016ed20413be84e87fc36ecd11d77e27862b
Dexter <sohambasak42@gmail.com>
2023-08-01 16:54:05 +0530

    git commit -m "first commit"

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
