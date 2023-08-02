db44e30fe12dd6c7981a01a6c40be74a9cfddda2
Dexter <sohambasak42@gmail.com>
2023-08-02 11:00:19 +0530

    feat: footer component & home page completed

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-content">
+        <h3 className="heading">REVIEWiT</h3>
+        <p>
+          Lights, Camera, Review - Your ultimate cinematic destination, where
+          honest critiques and passionate discussions unite, empowering you to
+          explore, rate, and discover the latest releases, transforming
+          movie-watching into an immersive and enlightening experience, while
+          connecting you with a vibrant community of fellow film enthusiasts.
+        </p>
+        <ul className="socials">
+          <li>
+            <a href="#">
+              <i className="fa fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-google-plus"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-youtube"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="footer-bottom">
+          <p>
+            copyright &copy;2023{" "}
+            <a href="https://github.com/soham-basak">Dexter</a>{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Footer;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Hero from "../../components/Hero/Hero";
 import Movies from "../../components/Movies/Movies";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <div className="home">
         <Hero />
         <Movies />
+        <Footer />
       </div>
     </>
   );
