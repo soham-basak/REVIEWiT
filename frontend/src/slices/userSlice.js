23099976647b1174fd3c156d1643c5b6c9cc627c
Dexter <sohambasak42@gmail.com>
2023-08-04 19:17:27 +0530

    feat(frontend): redux store and auth slices

---
+++ frontend/src/slices/userSlice.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "./apiSilce";
+
+const USERS_URL = "/api/users";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export {
+  useLoginMutation,
+  useRegisterMutation,
+  useLogoutMutation,
+  useUpdateUserMutation,
+};
