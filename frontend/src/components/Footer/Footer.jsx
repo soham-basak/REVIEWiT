389cb1ef055094bac9143b24f954261fbee02916
Dexter <sohambasak42@gmail.com>
2023-08-17 22:57:26 +0530

    fix footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -15,27 +15,27 @@ const Footer = () => {
         </p>
         <ul className="socials">
           <li>
-            <a href="#">
+            <a>
               <i className="fa fa-facebook"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a>
               <i className="fa fa-twitter"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a>
               <i className="fa fa-google-plus"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a>
               <i className="fa fa-youtube"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a>
               <i className="fa fa-linkedin-square"></i>
             </a>
           </li>
